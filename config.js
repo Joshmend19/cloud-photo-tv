d294d92e4beb469ca4f7e20300ad865aa2f49610
Joshmend19 <joshmendoza1924@gmail.com>
2026-09-25 17:53:34 -0600

    Add configuration constants for Supabase and EmailJS

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+export const SUPABASE_URL = https://ywxwpcubdrfrdjktpzlt.supabase.co/rest/v1/;
+export const SUPABASE_ANON_KEY = sb_publishable_D2yESJO6BdfsA5pumFaj4Q_l319uXL9;
 
+export const EMAILJS_PUBLIC_KEY = "PASTE_YOUR_EMAILJS_PUBLIC_KEY";
+export const EMAILJS_SERVICE_ID = "PASTE_YOUR_EMAILJS_SERVICE_ID";
+export const EMAILJS_TEMPLATE_ID = "PASTE_YOUR_EMAILJS_TEMPLATE_ID";
