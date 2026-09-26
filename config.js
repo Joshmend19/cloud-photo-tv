f0b3acafb166d553088c655b2cb888e680574b0b
Joshmend19 <joshmendoza1924@gmail.com>
2026-09-25 22:09:36 -0600

    Update EmailJS configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const SUPABASE_URL = "https://ywxwpcubdrfrdjktpzlt.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_D2yESJO6BdfsA5pumFaj4Q_l319uXL9";
 
-export const EMAILJS_PUBLIC_KEY = "PASTE_YOUR_EMAILJS_PUBLIC_KEY";
-export const EMAILJS_SERVICE_ID = "PASTE_YOUR_EMAILJS_SERVICE_ID";
-export const EMAILJS_TEMPLATE_ID = "PASTE_YOUR_EMAILJS_TEMPLATE_ID";
+export const EMAILJS_PUBLIC_KEY = "ZgNjiieNSWpzeyBtu";
+export const EMAILJS_SERVICE_ID = "service_aj2tx81";
+export const EMAILJS_TEMPLATE_ID = "template_zcd904l";
